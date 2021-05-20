2b2e4df49fb7513b828d046f8dde005b2edb7f89
Ulisses <m.stunik@gmail.com>
2021-05-20 09:19:34 -0300

    separate front and backend

--- frontend/src/components/SearchBox.jsx
+++ frontend/src/components/SearchBox.jsx
@@ -1,8 +1,6 @@
-function SearchBox() {
+export default function SearchBox() {
     return <div>
         <input type="text" name="searchEntry" id="searchEntry" placeholder="Pesquisar conteúdo" />
         <button>Q</button>
     </div>
 }
-
-export default SearchBox;
