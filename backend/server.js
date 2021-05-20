5705e19a1acc29b22555521aa5c79eb9986f873e
Ulisses <m.stunik@gmail.com>
2021-05-20 10:01:37 -0300

    add test music

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,15 @@ const app = express();
 const port = 3000;
 
 const exampleMusics = [
-	'music1':{
-		name:'music1',
-		author:'author1',
-		duration:180, //seconds
-		img:'https://images.unsplash.com/photo-1621360841013-c7683c659ec6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=pan-yunbo-EgL0EtzL0Wc-unsplash.jpg&w=640'
-	}
-]
+	{
+		music1: {
+			name: 'music1',
+			author: 'author1',
+			duration: 180, //seconds
+			img: 'https://images.unsplash.com/photo-1621360841013-c7683c659ec6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=pan-yunbo-EgL0EtzL0Wc-unsplash.jpg&w=640',
+		},
+	},
+];
 
 app.get('/music/0', (req, res) => {
 	res.send(exampleMusics[0]);
