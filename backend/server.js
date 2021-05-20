c39e7f990ae100f8738dfaf9869a64083c5acaf6
Ulisses <m.stunik@gmail.com>
2021-05-20 10:00:51 -0300

    add test music

--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,17 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-	res.send('Hello World!');
+const exampleMusics = [
+	'music1':{
+		name:'music1',
+		author:'author1',
+		duration:180, //seconds
+		img:'https://images.unsplash.com/photo-1621360841013-c7683c659ec6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=pan-yunbo-EgL0EtzL0Wc-unsplash.jpg&w=640'
+	}
+]
+
+app.get('/music/0', (req, res) => {
+	res.send(exampleMusics[0]);
 });
 
 app.listen(port, () => {
