8988882470e64c67ec1b86cbef49601f1e47c8e4
Ulisses <m.stunik@gmail.com>
2021-05-20 10:36:26 -0300

    add ItemCard component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import SearchBox from './components/SearchBox'
+import ItemCard from './components/ItemCard'
 
 function App() {
 	return <div className='App'>
 		<SearchBox></SearchBox>
+		<ItemCard></ItemCard>
 	</div>;
 }
 
---
+++ frontend/src/components/ItemCard.jsx
@@ -0,0 +1,15 @@
+import './ItemCard.css'
+
+export default function ItemCard() {
+    const size = 8
+    const testImage = 'https://images.unsplash.com/photo-1621360841013-c7683c659ec6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=pan-yunbo-EgL0EtzL0Wc-unsplash.jpg&w=640'
+    return <div>
+        <div className="imageWrapper" style={{ "background-image": `url(${testImage})`, width: size + 'rem', height: size + 'rem' }}>
+
+        </div>
+        <div id="name">
+            Music name
+        </div>
+        <div>Author</div>
+    </div >
+}
