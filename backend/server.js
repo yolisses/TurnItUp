3289629cc952caa29280afba8c772fe2d34c57f7
Ulisses <m.stunik@gmail.com>
2021-05-21 09:30:57 -0300

    change test response

--- backend/server.js
+++ backend/server.js
@@ -6,12 +6,10 @@ const port = 5000;
 
 const exampleMusics = [
 	{
-		music1: {
-			name: 'music1',
-			author: 'author1',
-			duration: 180, //seconds
-			img: 'https://images.unsplash.com/photo-1621360841013-c7683c659ec6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=pan-yunbo-EgL0EtzL0Wc-unsplash.jpg&w=640',
-		},
+		name: 'music1',
+		author: 'author1',
+		duration: 180, //seconds
+		img: 'https://images.unsplash.com/photo-1621360841013-c7683c659ec6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=pan-yunbo-EgL0EtzL0Wc-unsplash.jpg&w=640',
 	},
 ];
 
