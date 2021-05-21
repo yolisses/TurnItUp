31fe8b9b827962c00e209151b4a2d2ed09e43d97
Ulisses <m.stunik@gmail.com>
2021-05-21 10:22:02 -0300

    change from img to image in response

--- backend/server.js
+++ backend/server.js
@@ -7,9 +7,10 @@ const port = 5000;
 const exampleMusics = [
 	{
 		name: 'music1',
-		author: 'author1',
+		author: 'aufdsfasdfasdfadfasdfasdfadfasdfdasdfthor1',
 		duration: 180, //seconds
-		img: 'https://images.unsplash.com/photo-1621360841013-c7683c659ec6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=pan-yunbo-EgL0EtzL0Wc-unsplash.jpg&w=640',
+		image:
+			'https://images.unsplash.com/photo-1621360841013-c7683c659ec6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=pan-yunbo-EgL0EtzL0Wc-unsplash.jpg&w=640',
 	},
 ];
 
