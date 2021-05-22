c8dc29fbae375953be14ca44816c1b21a679b1ab
Ulisses <m.stunik@gmail.com>
2021-05-22 09:57:04 -0300

    give exampleMusics dinamic length

--- backend/exampleMusics.js
+++ backend/exampleMusics.js
@@ -1,11 +1,13 @@
-const exampleMusics = [
-	{
-		name: 'music1',
-		author: 'aufdsfasdfasdfadfasdfasdfadfasdfdasdfthor1',
+const exampleMusics = [];
+
+for (let i = 0; i < 20; i++) {
+	exampleMusics.push({
+		name: 'music' + i,
+		author: 'author' + i,
 		duration: 180, //seconds
 		image:
 			'https://images.unsplash.com/photo-1621360841013-c7683c659ec6?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&dl=pan-yunbo-EgL0EtzL0Wc-unsplash.jpg&w=640',
-	},
-];
+	});
+}
 
 export default exampleMusics;
