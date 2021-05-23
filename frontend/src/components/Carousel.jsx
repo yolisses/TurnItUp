63ee467d188b67506d41be34e853b0186e44cc39
Ulisses <m.stunik@gmail.com>
2021-05-23 09:44:55 -0300

    fix recursive loading of data

--- frontend/src/components/Carousel.jsx
+++ frontend/src/components/Carousel.jsx
@@ -7,10 +7,12 @@ import './Carousel.css'
 function Carousel(params) {
     const [itens, setItens] = useState([])
 
-    api.get(params.src)
-        .then((res) => {
-            setItens(res.data)
-        })
+    if (itens.length === 0) {
+        api.get(params.src)
+            .then((res) => {
+                setItens(res.data)
+            })
+    }
 
     return <div className="carousel vertical_margin">
         {itens.map(item => <ItemCard id={item} key={item}></ItemCard>)}
