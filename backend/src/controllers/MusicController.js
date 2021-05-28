8ebb7af0aa55a77411a74decc55f54c6930bd748
Ulisses <m.stunik@gmail.com>
2021-05-28 11:32:50 -0300

    add update function

--- backend/src/controllers/MusicController.js
+++ backend/src/controllers/MusicController.js
@@ -12,6 +12,19 @@ export default {
 		return res.json(musics);
 	},
 
+	async update(req, res) {
+		const { id } = req.params;
+		const { name, duration } = req.body;
+		const music = await Music.findByPk(id);
+		if (name) {
+			await music.update({ name });
+		}
+		if (duration) {
+			await music.update({ duration });
+		}
+		return res.json(music);
+	},
+
 	async remove(req, res) {
 		const { id } = req.params;
 		const musics = await Music.destroy({
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -7,5 +7,6 @@ const routes = express.Router();
 routes.get('/music', MusicController.index);
 routes.post('/music', MusicController.add);
 routes.delete('/music/:id', MusicController.remove);
+routes.put('/music/:id', MusicController.update);
 
 export default routes;
